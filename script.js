84ce71415d8521aade5abc3321cf447c0fa3c123
VinodMali <vinodxmali@gmail.com>
2026-07-13 23:43:42 +0530

    vinod mali

---
+++ script.js
@@ -0,0 +1,19 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+const typed = new Typed('.multiple-text', {
+      strings: ['Frontend Developer', 'Backend Developer', 'Web Designer', 'UI/UX Designer', 'App Developer'],
+      typeSpeed: 50,
+      backspeed: 50,
+      backDelay: 1200,
+      loop: true,
+});
